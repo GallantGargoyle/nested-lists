644f9c10e0a7476d5383562fea4244c783346554
GallantGargoyle <159750867+GallantGargoyle@users.noreply.github.com>
2024-12-29 11:24:29 +0530

    Update spreadsheets.py

--- spreadsheets.py
+++ spreadsheets.py
@@ -10,9 +10,28 @@ def read_csv(file_path):
             contents.append(row)
         return contents
 
+def user_input():
+    boolean_guard = True
+    while boolean_guard:
+        #getting user input
+        command = input("Enter command: ")
+
+        #if-else ladder dealing with different commands
+        if command.startswith("exit"):
+            boolean_guard = False
+        elif command.startswith("add"):
+            continue
+        elif command.startswith("sortby"):
+            continue
+        elif command.startswith("print"):
+            continue
+        else:
+            print("Invalid command: " + command)
+
 if __name__ == "__main__":
     # Check if the spreadsheet file was passed in
     if len(sys.argv) > 1:   
         contents = read_csv(sys.argv[1])
+        user_input()
     else:
         print("Needs a spreadsheet file")
